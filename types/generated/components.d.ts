ca631f48b0485bc94f9ac7176bc8ca50857757cb
vaseemtsg <vaseems@techsprintglobal.com>
2025-07-02 19:00:49 +0530

    add header footer

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -38,6 +38,29 @@ export interface LayoutFeatureSection extends Struct.ComponentSchema {
   };
 }
 
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    logoText: Schema.Attribute.Component<'components.link', false>;
+    socialLink: Schema.Attribute.Component<'components.link', true>;
+    text: Schema.Attribute.Text;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    ctaButton: Schema.Attribute.Component<'components.link', false>;
+    logoText: Schema.Attribute.Component<'components.link', false>;
+  };
+}
+
 export interface LayoutHeroSection extends Struct.ComponentSchema {
   collectionName: 'components_layout_hero_sections';
   info: {
@@ -57,6 +80,8 @@ declare module '@strapi/strapi' {
       'components.feature': ComponentsFeature;
       'components.link': ComponentsLink;
       'layout.feature-section': LayoutFeatureSection;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
       'layout.hero-section': LayoutHeroSection;
     }
   }
